1cba2f4bd255192112ab38cf89ad5b3c84227227
v1n1btt <vbbotte@gmail.com>
2025-09-07 18:49:50 -0300

    Renderização condicional nas diferentes seções da página

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { act, useState } from 'react'
 import { useEffect } from 'react'
 import Header from './Header'
 import Splash from './Splash'
@@ -9,20 +9,25 @@ import Projects from './Projects'
 import Contact from './Contact'
 import Footer from './Footer'
 
+const tabs = {
+  index: "inicio",
+  about: "sobre",
+  team: "equipe",
+  projects: "projetos",
+  contact: "contato",
+}
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [activeTab, setActiveTab] = useState(tabs.index);
 
   return (
     <>
-      <Header/>
-      <section className="splash-screen-octobit">
-        <Splash/>
-        <MatrixBackground/>
-      </section>
-      <About/>
-      <section id="equipe"><Team/></section>
-      <section id="projetos"><Projects/></section>
-      <section id="contato"><Contact/></section>
+      <Header setActiveTab={setActiveTab} tabs={tabs}/>
+      {activeTab === tabs.index && <section className="splash-screen-octobit"><Splash/><MatrixBackground/></section>}
+      {activeTab === tabs.about && <About/>}
+      {activeTab === tabs.team && <section id="equipe"><Team/></section>}
+      {activeTab === tabs.projects && <section id="projetos"><Projects/></section>}
+      {activeTab === tabs.contact && <section id="contato"><Contact/></section>}
       <Footer/>
     </>
   )
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,17 +1,17 @@
 import logo from "./assets/img/Logo_OCTOBIT_Logo_Branco_VERTICAL.png"
 
-export default function () {
+export default function (props) {
     return (
         <header className="header" id="nav_bar">
-            <a href="./index.html" className="nav_logo_a"><img src={logo} alt="Logo branco vertical da Octobit Jr." className="nav_logo"/></a>
+            <a onClick={() => props.setActiveTab(props.tabs.index)} className="nav_logo_a"><img src={logo} alt="Logo branco vertical da Octobit Jr." className="nav_logo"/></a>
 
             <nav className="nav_bar">
                 <ul className="nav_bar_desktop">
-                    <li className="nav_bar_li"><a href="./index.html">Início</a></li>
-                    <li className="nav_bar_li"><a href="#sobre">Sobre nós</a></li>
-                    <li className="nav_bar_li"><a href="#equipe">Equipe</a></li>
-                    <li className="nav_bar_li"><a href="#projetos">Projetos</a></li>
-                    <li className="nav_bar_li"><a href="#contato">Contato</a></li>
+                    <li className="nav_bar_li"><a onClick={() => props.setActiveTab(props.tabs.index)}>Início</a></li>
+                    <li className="nav_bar_li"><a onClick={() => props.setActiveTab(props.tabs.about)}>Sobre nós</a></li>
+                    <li className="nav_bar_li"><a onClick={() => props.setActiveTab(props.tabs.team)}>Equipe</a></li>
+                    <li className="nav_bar_li"><a onClick={() => props.setActiveTab(props.tabs.projects)}>Projetos</a></li>
+                    <li className="nav_bar_li"><a onClick={() => props.setActiveTab(props.tabs.contact)}>Contato</a></li>
                 </ul>
             </nav>
         </header>
