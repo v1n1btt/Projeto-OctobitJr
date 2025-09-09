d68f1361c05c407482487de0ffe9fa935352ed63
v1n1btt <vbbotte@gmail.com>
2025-09-09 18:14:51 -0300

    Correção de bug em que o ícone do toggle não aparece

--- src/ToggleDarkMode.jsx
+++ src/ToggleDarkMode.jsx
@@ -1,5 +1,8 @@
+import dayAndNightWhite from "./assets/img/day-and-night-white.png"
+import dayAndNight from "./assets/img/day-and-night.png"
+
 export default function (props) {
     return (
-        <button className="toggle" style={{backgroundColor: props.darkMode ? '#2b2b2b' : '#dbdad9', backgroundImage: props.darkMode ? "url('src/assets/img/day-and-night-white.png')" : "url('src/assets/img/day-and-night.png')"}} onClick={function () {props.setDarkMode(!props.darkMode); document.getElementById("body").classList.add("light")}}></button>
+        <button className="toggle" style={{backgroundColor: props.darkMode ? '#2b2b2b' : '#dbdad9', backgroundImage: props.darkMode ? `url(${dayAndNightWhite})` : `url(${dayAndNight})`}} onClick={function () {props.setDarkMode(!props.darkMode); document.getElementById("body").classList.add("light")}}></button>
     )
 }
